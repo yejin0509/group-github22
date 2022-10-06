2c9c5cca525029ab297c1f37595c484582dc837a
jherin <jhr6454@naver.com>
2022-10-06 09:49:43 +0900

    문제 틀 구현 피드백 전 코드

---
+++ app/src/main/java/org/techtown/test/MainActivity.java
@@ -0,0 +1,21 @@
+package org.techtown.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+}
