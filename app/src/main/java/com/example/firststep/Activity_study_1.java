171b212dca0e093ccb0c1d60384ab603941c4b6b
dbwjd <dbwjd20111@naver.com>
2022-11-16 21:44:55 +0900

    학습하기 화면 기능 추가

--- app/src/main/java/com/example/firststep/Activity_study_1.java
+++ app/src/main/java/com/example/firststep/Activity_study_1.java
@@ -42,8 +42,11 @@ public class Activity_study_1 extends AppCompatActivity {
         TextView textView3 = (TextView) findViewById(R.id.textView3);
         ImageView imageView2 = (ImageView) findViewById(R.id.imageView2);
         TextView editTextNumber = (TextView) findViewById(R.id.editTextNumber);
+        Button backStudy = findViewById(R.id.button8);
 
         itemArraylist = dbSuppormer.getResult(categoryname);
+
+        backStudy.setVisibility(View.GONE);
 
 
         textView3.setText(categoryname);
@@ -51,6 +54,12 @@ public class Activity_study_1 extends AppCompatActivity {
         imageView2.setImageBitmap(itemArraylist.get(n).getImage());
 
         Log.i("답변", String.valueOf(n)+ itemArraylist.get(n).getAnswer());
+
+        if(n == 0){
+
+        } else {
+
+        }
 
 
         Button Button2 = (Button) findViewById(R.id.button2);
@@ -68,6 +77,7 @@ public class Activity_study_1 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 n += 1;
+                backStudy.setVisibility(View.VISIBLE);
 
                 if(n < itemArraylist.size()){
                     Log.i("답변", String.valueOf(n)+ itemArraylist.get(n).getAnswer());
@@ -81,24 +91,21 @@ public class Activity_study_1 extends AppCompatActivity {
             }
         });
 
-        Button backStudy = findViewById(R.id.button8);
+
         backStudy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 n -= 1;
 
-                if(n < itemArraylist.size()){
+                if(n < itemArraylist.size() && n > 0){
                     Log.i("답변", String.valueOf(n)+ itemArraylist.get(n).getAnswer());
                     editTextNumber.setText(itemArraylist.get(n).getAnswer());
                     imageView2.setImageBitmap(itemArraylist.get(n).getImage());
-                } else if(n <= 0) {
-                    Log.i("문제끝", String.valueOf(n));
-                    Intent intent = new Intent(getApplicationContext(), Class_list.class);
-                    startActivity(intent);
-                } else {
-                    Log.i("문제끝", String.valueOf(n));
-                    Intent intent = new Intent(getApplicationContext(), Class_list.class);
-                    startActivity(intent);
+                } else if(n == 0){
+                    backStudy.setVisibility(View.GONE);
+                    Log.i("답변", String.valueOf(n)+ itemArraylist.get(n).getAnswer());
+                    editTextNumber.setText(itemArraylist.get(n).getAnswer());
+                    imageView2.setImageBitmap(itemArraylist.get(n).getImage());
                 }
             }
         });
